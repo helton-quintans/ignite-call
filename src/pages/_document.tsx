26611bf8e91b8dd06fd1f622f73614c7ca7ab3ec
helton-quintans <helton.quit@gmail.com>
2023-02-14 18:29:48 -0300

    config tag styles for stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from "@ignite-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -6,11 +7,13 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        
+
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+
+        <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText()}}></style>
       </Head>
       <body>
         <Main />
