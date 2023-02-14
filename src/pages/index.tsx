d2f2400a7fc73543749b345acbc6ff66dda70bd4
helton-quintans <helton.quit@gmail.com>
2023-02-14 18:38:02 -0300

    chore: lint code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { Heading } from "@ignite-ui/react";
+import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
-  return <Heading>Hello</Heading>;
+  return <Heading>Hello</Heading>
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,17 +1,15 @@
-import { globalCss } from "@ignite-ui/react";
+import { globalCss } from '@ignite-ui/react'
 
 export const globalStyles = globalCss({
-  "*": {
-    boxSizing: "border-box",
+  '*': {
+    boxSizing: 'border-box',
     padding: 0,
     margin: 0,
   },
 
   body: {
-    backgroundColor:'$gray900',
+    backgroundColor: '$gray900',
     color: '$gray100',
-    '-webkit-font-smoothing': 'antialiased'
-  }
-
-  
-});
+    '-webkit-font-smoothing': 'antialiased',
+  },
+})
