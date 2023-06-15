c88e20e60d83add9d190279052f9ea1db8f563e3
helton-quintans <helton.quit@gmail.com>
2023-06-15 00:49:04 -0300

    feat [register]: validade form with zod

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,8 +1,44 @@
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
-import { Container, Form, Header } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Container, Form, FormError, Header } from './styles'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Precisa ter no mínimo 3 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, { message: 'Apenas letras e hifens' })
+    .transform((username) => username.toLowerCase),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data)
+    // try {
+    //   //   const response = await api.post('/users', {
+    //   //     name: formState.name,
+    //   //     email: formState.email,
+    //   //     password: formState.password
+    //   //   })
+    // } catch (error) {
+    //   console.log(error)
+    // }
+  }
   return (
     <Container>
       <Header>
@@ -14,18 +50,28 @@ export default function Register() {
 
         <MultiStep size={4} currentStep={1} />
 
-        <Form as="form">
+        <Form as="form" onSubmit={handleSubmit(handleRegister)}>
           <label>
             <Text size="sm">Nome do usuário</Text>
-            <TextInput prefix="hq.com/" placeholder="seu-usuário" />
+            <TextInput
+              prefix="hq.com/"
+              placeholder="seu-usuário"
+              {...register('username')}
+            />
+            {errors.username && (
+              <FormError size="sm">{errors.username.message}</FormError>
+            )}
           </label>
 
           <label>
             <Text size="sm">Nome completo</Text>
-            <TextInput placeholder="seu nome" />
+            <TextInput placeholder="seu nome" {...register('name')} />
+            {errors.name && (
+              <FormError size="sm">{errors.name.message}</FormError>
+            )}
           </label>
 
-          <Button type="submit">
+          <Button type="submit" disabled={isSubmitting}>
             Próximo Passo
             <ArrowRight />
           </Button>
--- src/pages/register/styles.ts
+++ src/pages/register/styles.ts
@@ -3,7 +3,7 @@ import { Box, Heading, Text, styled } from '@ignite-ui/react'
 export const Container = styled('main', {
   maxWidth: 572,
   margin: '$20 auto $4',
-  padding: '$2 $4',
+  padding: '0 $4',
   borderRadius: '8px',
 })
 
@@ -31,4 +31,8 @@ export const Form = styled(Box, {
     flexDirection: 'column',
     gap: '$2',
   },
+})
+
+export const FormError = styled(Text, {
+  color: '#f75a68',
 })
