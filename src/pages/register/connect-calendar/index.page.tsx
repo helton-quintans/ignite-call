d4949804deb4842e5025e27c782993e412a8ec11
helton-quintans <helton.quit@gmail.com>
2023-06-20 15:22:34 -0300

    feat: create connect-calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,45 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+// import { NextSeo } from 'next-seo'
+// import { useRouter } from 'next/router'
+
+import { ArrowRight } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function ConnectCalendar() {
+  // const router = useRouter()
+
+  // async function handleRegister(data: RegisterFormData) {}
+
+  return (
+    <>
+      {/* <NextSeo title="Conectar com Google Calendar | Hq Call" /> */}
+
+      <Container>
+        <Header>
+          <Heading as="strong">Conecte sua agenda</Heading>
+          <Text>
+            Conecte o seu calendário para verificar automaticamente as horas
+            ocupadas e os novos eventos à medida em que são agendados.
+          </Text>
+
+          <MultiStep size={4} currentStep={2} />
+        </Header>
+
+        <ConnectBox>
+          <ConnectItem>
+            <Text>Google Calendar</Text>
+            <Button variant="secondary" size="sm">
+              Conectar
+              <ArrowRight />
+            </Button>
+          </ConnectItem>
+          <Button>
+            Próximo passo
+            <ArrowRight />
+          </Button>
+        </ConnectBox>
+      </Container>
+    </>
+  )
+}
