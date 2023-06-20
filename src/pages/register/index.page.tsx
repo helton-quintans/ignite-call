fe5cd8e103aeff78024d7bebc80c0885cd4ab18b
helton-quintans <helton.quit@gmail.com>
2023-06-20 15:23:47 -0300

    feat: redirect user  router.push('/register/connect-calendar')

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -51,7 +51,7 @@ export default function Register() {
         username: data.username,
       })
 
-      // await router.push('/register/connect-calendar')
+      await router.push('/register/connect-calendar')
     } catch (err) {
       if (err instanceof AxiosError && err?.response?.data?.message) {
         alert('Este usuário já existe.')
